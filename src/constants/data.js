68a816aa688a94f269d0ba9d3c78a9e850950703
Armando <armandocrescencio343@gmail.com>
2024-02-05 09:47:14 -0600

    ADD: constants files

---
+++ src/constants/data.js
@@ -0,0 +1,10 @@
+const paths = [
+  { path: '/', item: 'Home' },
+  { path: 'kardex', item: 'Kardex' },
+  { path: 'schedule', item: 'Schedule' },
+  { path: 'login', item: 'Log Out' },
+]
+
+export {
+  paths
+}
---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+import { paths } from './data'
+
+export {
+  paths
+}
