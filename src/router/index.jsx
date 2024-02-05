1726b2589885ae1e0dfa33ef9f1853cea425a6e3
Armando <armandocrescencio343@gmail.com>
2024-02-04 21:26:52 -0600

    SET: browser router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,30 +1,46 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import { Home, Kardex, Login, NotFound, Schedule } from '../pages'
 import App from '../App'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='/' index element={<Home />} />
+      <Route path='kardex' element={<Kardex />} />
+      <Route path='schedule' element={<Schedule />} />
+      <Route path='login' element={<Login />} />
+      <Route path='*' element={<NotFound />} />
+    </Route>
+  )
+)
+
+// export const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <Home />,
          
-      },
-      {
-        path: 'kardex',
-        element: <Kardex />
-      },
-      {
-        path: 'login',
-        element: <Login />
-      },
-      {
-        path: 'schedule',
-        element: <Schedule />
-      }
-    ]
-  }
-])
+//       },
+//       {
+//         path: 'kardex',
+//         element: <Kardex />
+//       },
+//       {
+//         path: 'login',
+//         element: <Login />
+//       },
+//       {
+//         path: 'schedule',
+//         element: <Schedule />
+//       },
+//       {
+//         path: 'notfound',
+//         element: <NotFound />
+//       }
+//     ]
+//   }
+// ])
