a89ed8663be450971f6d482d11f8972a0b7daf1e
Armando <armandocrescencio343@gmail.com>
2024-02-05 09:44:30 -0600

    MODIFIED: Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { Nav } from '../components'
 
-const paths = [
-  { path: '/', item: 'Home'},
-  { path: 'kardex', item: 'Kardex'},
-  { path: 'schedule', item: 'Schedule'},
-  { path: 'login', item: 'Log Out'},
-]
-
 export function Layout () {
   return (
     <>
