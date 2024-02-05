cac10108141b1f34df994bb2001b7e120b5a4558
Armando <armandocrescencio343@gmail.com>
2024-02-05 09:43:39 -0600

    ADD: Nav to the Layout

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,29 @@
-
-export default function Nav () {
+export default function Nav() {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className='w-full h-16 bg-gradient-to-r from-gray-900 to-gray-700/80 bg-transparen backdrop-blur-sm'>
+      <section className='flex items-center justify-between w-11/12 h-full mx-auto max-w-7xl'>
+        <article className='flex flex-row items-center gap-4 space-y-1.5'>
+          <Link
+            to='/'
+            className='text-2xl text-white font-semibold leading-none tracking-tight flex items-center hover:text-cyan-400'
+          >
+            Sitec 2.0
+          </Link>
+          <p className='text-sm text-white '>Escolar System</p>
+        </article>
+        <ul className='flex flex-row gap-8 scale-up-center mr-8 text-white'>
+          {paths.map(({ item, path }, index) => (
+            <li key={index}>
+              <NavLink
+                to={path}
+                className='text-sm font-medium hover:text-cyan-400 transition-colors duration-500'
+              >
+                {item}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </nav>
   )
 }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,9 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import { Nav } from '../components'
+
+const paths = [
+  { path: '/', item: 'Home'},
+  { path: 'kardex', item: 'Kardex'},
+  { path: 'schedule', item: 'Schedule'},
+  { path: 'login', item: 'Log Out'},
+]
 
 export function Layout () {
   return (
     <>
-      <nav>nav</nav>
+      <Nav />
       <main>
         <Outlet />
       </main>
